d8eef811744e153acb93b48564332da2096658a0
Yulia Gutorova <j.gutorova@gmail.com>
2023-03-18 20:26:48 +0100

    glow effect in header

--- src/components/homePage/components/HeaderSection.tsx
+++ src/components/homePage/components/HeaderSection.tsx
@@ -8,7 +8,7 @@ const HeaderSection = () => {
             <div className={classes.headerSectionContentWrapper}>
                 <div className={classes.headerSectionContent}>
                     <p className={classes.headerSectionWelcome}> WELCOME TO OUR WEBSITE </p>
-                    <h1 className={classes.headerSectionTitle}>Stockholm's premier home cleaning service</h1>
+                    <h1 className={`${classes.headerSectionTitle} ${classes.glow}`}>Stockholm's premier home cleaning service</h1>
                     <p className={classes.headerSectionText}>Hire us! Get your home cleaned. We are a professional cleaning company offering all types of cleaning services.</p>
                 </div>
                 <div className={classes.headerSectionButtonWrapper}>
